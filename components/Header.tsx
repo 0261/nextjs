3e38d8e53daa9a63be856a2544412f129eaa7363
0261 <bada013@naver.com>
2019-04-27 11:21:14 +0900

    delete styledcomponent

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,10 @@
 import Link from 'next/link';
-import styled from "styled-components";
 import React from 'react';
 
-const LinkStyle = styled.a`
-    margin-right: 1rem;
-`
 
 export const Header = () => {
     return (
         <div>
-            <Link href="/" ><LinkStyle>홈</LinkStyle></Link>
-            <Link as={`/boards`} href={{
-                pathname: '/board',
-                query:{
-                    name: 'padakim'
-                }
-            }}><LinkStyle>게시판</LinkStyle></Link>
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,69 @@
+import '../src/bootstrap';
 import React from 'react';
-import { Layout } from '@components/Layout';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+import Link from 'next/link';
 
+import { Layout } from '@components/Layout';
+const useStyles = makeStyles(theme => ({
+	root: {
+	  textAlign: 'center',
+	  paddingTop: theme.spacing.unit * 20,
+	},
+}));
 interface Props {
 	text: string;
 	name: string;
 }
 
-const Index:React.FunctionComponent<Props> = ({text, name}) => (
-	<Layout>
-		<div>
-			<h1> {text} Welcome {name}!</h1>
-		</div>
-	</Layout>
-)
+const Index:React.FunctionComponent<Props> = ({text, name}) => {
+	const classes = useStyles({});
+	const [open, setState] = React.useState(false);
+  
+	const handleClose = () => {
+	  setState(false);
+	};
+	const handleClick = () => {
+	  setState(true);
+	};
+  
+	return (
+		<Layout>
+			<div className={classes.root}>
+				<Dialog open={open} onClose={handleClose}>
+				<DialogTitle>Super Secret Password</DialogTitle>
+				<DialogContent>
+					<DialogContentText>1-2-3-4-5</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button color="primary" onClick={handleClose}>
+					OK
+					</Button>
+				</DialogActions>
+				</Dialog>
+				<Typography variant="h4" gutterBottom>
+				Material-UI
+				</Typography>
+				<Typography variant="subtitle1" gutterBottom>
+				example project
+				</Typography>
+				<Typography gutterBottom>
+				<Link href="/about">
+					<a>Go to the about page</a>
+				</Link>
+				</Typography>
+				<Button variant="contained" color="secondary" onClick={handleClick}>
+				Super Secret Password
+				</Button>
+			</div>
+		</Layout>
+	);
+}
 
 export default Index;
